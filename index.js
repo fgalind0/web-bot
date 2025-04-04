7d460bce55a9a8787d6ce6eb2aefd6f21b907ac4
Fernando Galindo <fernando.galindo1394@gmail.com>
2025-04-03 23:07:42 -0400

    working version

--- index.js
+++ index.js
@@ -2,38 +2,40 @@ import puppeteer from 'puppeteer';
 import {setTimeout} from "node:timers/promises";
 
 // just need the url to channel we want
-const targetLoungeChannel = 'TBD';
+const targetLoungeChannel = 'https://revolt.onech.at/server/01JDKH82R0RHG2VF9YDWKEFHC5/channel/01JDKJX5E3370PBN8121999MND';
 const spoofLoungeChannel = 'https://app.revolt.chat/server/01JQT731F1T231HEBZ709M46V1/channel/01JQT7E1V0DHAQF2X5KPNDZVDW';
 const claim = '/claim';
 
-// ATTENTION - in command prompt run the following command without leading //
+// STEPS
+// 1.) in command prompt run the following command without leading 
 // start chrome --remote-debugging-port=9222
+// 2.) login to revolt chat in the browser that was opened
+// 3.) open terminal ctrl + ` (tilda to the left of 1 key)
+// 4.) run below command 
+// node index.js
 const browserURL = 'http://127.0.0.1:9222';
-const browser = await puppeteer.connect({browserURL});
+const browser = await puppeteer.connect({browserURL, defaultViewport: null,});
 const page = await browser.newPage();
 
 // TBD change below to targetLoungeChannel
-await page.goto(spoofLoungeChannel);
-// let page load
-await setTimeout(4000);
+await page.goto(targetLoungeChannel);
 
 // will run infinitely
 while (true){
     // try to get a ticket
     const ticket = await getTicket(page);
     if (ticket){
-        // if we have a ticket we click on it
-        await ticket.click();
-        // enter claim message
-        await page.locator('#message').fill(claim);
-        // press 'Enter' on keyboard
-        await page.keyboard.press('Enter');
+        const successful = await claimTicket(ticket);
         // Log the progress
-        console.log('$$$ Claimed $$$');
-        console.log('Waiting for cool off');
-        // have a cool down
-        await setTimeout(1*20*1000); // testing cool down
-        // await setTimeout(7*60*1000); // prod cool down
+        if (successful) {
+            console.log('$$$ Claimed $$$');
+            console.log('Waiting for cool off');
+            // have a cool down
+            // await setTimeout(1*20*1000); // testing cool down
+            await setTimeout(7*60*1000); // prod cool down
+        } else {
+            console.log('Some one else claimed :(')
+        }
         console.log('Going again')
     } else {
         // we do not have a ticket and we try again after half second pause
@@ -47,11 +49,28 @@ async function getTicket(page) {
     try {
         // will try to load ticket for 10 seconds. If it finds a ticket will return to be claimed
         // ATTENTION - Potential failure point if selector needs to be changed (because of channel name has different casing)
-        return await page.waitForSelector('div ::-p-text(ticket)', {timeout: 10_000}); // wait for 30 seconds
+        return await page.waitForSelector('div ::-p-text(ticket-)', {timeout: 15_000}); // wait for 30 seconds
       } catch (error) {
-        // we hit the 10 second timeout and return false so we don't try to click on nonexistent things
-        console.log('no ticket found in past 10 seconds')
+        // we hit the 15 second timeout and return false so we don't try to click on nonexistent things
+        console.log('no ticket found in past 15 seconds')
         return false;
       }
       
+}
+
+async function claimTicket(ticket) {
+    try {
+        // if we have a ticket we click on it
+        await setTimeout(200);
+        await ticket.click();
+        // enter claim message
+        await page.locator('#message').fill(claim);
+        // wait half a second before pressing 'Enter' on keyboard
+        await page.keyboard.press('Enter');
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+    
 }
